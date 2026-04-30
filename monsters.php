36f4eaf3c1545993189244025b1de1c960ace94f
Orion Ludwick <Tia.ludwick@Maine.edu>
2026-04-30 04:22:27 +0000

    update

--- monsters.php
+++ monsters.php
@@ -1,22 +1,97 @@
 <?php
 $allMonsters = [
+
+    // Starter monsters
     [
-        "name" => "Flameling",
+        "name" => "Emberling",
         "max_hp" => 50,
         "attack" => 12,
         "level" => 1
     ],
+    // Small floating fire sprite with glowing coal eyes and flickering tail flames
+
     [
-        "name" => "Aquabub",
+        "name" => "Tidepup",
         "max_hp" => 60,
         "attack" => 10,
         "level" => 1
     ],
+    // Playful water beast resembling a baby otter made of flowing water and bubbles
+
     [
-        "name" => "Stonehoof",
+        "name" => "Gravhorn",
         "max_hp" => 70,
         "attack" => 8,
         "level" => 1
     ],
+    // Heavy baby rock creature with tiny tusks and cracked stone armor plates
+
+
+    // Greater type monsters
+    [
+        "name" => "Voltclaw",
+        "max_hp" => 85,
+        "attack" => 18,
+        "level" => 3
+    ],
+    // Sleek electric feline with glowing blue claws and crackling fur
+
+    [
+        "name" => "Frostfangor",
+        "max_hp" => 95,
+        "attack" => 16,
+        "level" => 3
+    ],
+    // Ice-armored wild boar with frozen tusks and mist constantly trailing from its breath
+
+    [
+        "name" => "Cindeem",
+        "max_hp" => 110,
+        "attack" => 14,
+        "level" => 4
+    ],
+    // Large humanoid rock construct with molten lava glowing through its cracks
+
+    [
+        "name" => "Venomflare",
+        "max_hp" => 80,
+        "attack" => 20,
+        "level" => 4
+    ],
+    // Winged serpent with toxic green fire dripping from its mouth and veins
+
+
+    // Ancient type monsters
+    [
+        "name" => "Hydraskorn",
+        "max_hp" => 180,
+        "attack" => 30,
+        "level" => 7
+    ],
+    // Massive hydra with multiple horned heads and glowing abyssal water flowing between scales
+
+    [
+        "name" => "Celestyr",
+        "max_hp" => 160,
+        "attack" => 34,
+        "level" => 7
+    ],
+    // Elegant dragon made of starlight and crystal feathers, constantly shimmering like a galaxy
+
+    [
+        "name" => "Root Titan",
+        "max_hp" => 220,
+        "attack" => 25,
+        "level" => 8
+    ],
+    // Ancient tree giant with moving bark armor and glowing green runes in its core
+
+    [
+        "name" => "Void Requiem",
+        "max_hp" => 150,
+        "attack" => 38,
+        "level" => 8
+    ]
+    // Floating shadow entity with no face, only shifting black mist and faint screaming light
 ];
 ?>
